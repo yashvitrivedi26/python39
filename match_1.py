31e70f072fa0a8ec346ff3046e36a92494f7d15d
Yashvi Trivedi <166053460+yashvitrivedi26@users.noreply.github.com>
2026-08-22 12:01:39 +0530

    Match_Case Exercise

---
+++ match_1.py
@@ -0,0 +1,29 @@
+#write a program to accept month number from user and display name of month using match case 
+
+month = int(input("Enter Month Number:"))
+
+match month:
+    case 1:
+        print('January')
+    case 2:
+        print('February')
+    case 3:
+        print('March')
+    case 4:
+        print('April')
+    case 5:
+        print('May')
+    case 6:
+        print('June')
+    case 7:
+        print('July')
+    case 8:
+        print('August')
+    case 9:
+        print('September')
+    case 10:
+        print('October')
+    case 11:
+        print('November')
+    case 12:
+        print('December')
---
+++ match_2.py
@@ -0,0 +1,29 @@
+#write a program to accept month number from user and display name of month using match case 
+
+month = int(input("Enter Month Number:"))
+
+match month:
+    case 1:
+        print('Winter')
+    case 2:
+        print('Winter')
+    case 3:
+        print('Spring')
+    case 4:
+        print('Spring')
+    case 5:
+        print('Summer')
+    case 6:
+        print('Summer')
+    case 7:
+        print('Monsoon')
+    case 8:
+        print('Monsoon')
+    case 9:
+        print('Monsoon')
+    case 10:
+        print('Autumn')
+    case 11:
+        print('Autumn')
+    case 12:
+        print('Winter')
